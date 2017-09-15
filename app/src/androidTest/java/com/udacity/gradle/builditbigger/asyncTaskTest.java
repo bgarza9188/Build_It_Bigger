25164b86753322de099d42f5d792f8de33cdd51a
Ben <bgarza9188@gmail.com>
2017-09-14 21:23:32 -0500

    Completed Step 4

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/asyncTaskTest.java
@@ -0,0 +1,40 @@
+package com.udacity.gradle.builditbigger;
+
+/**
+ * Created by Ben on 9/14/2017.
+ */
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static org.hamcrest.core.AllOf.allOf;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class asyncTaskTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void testAsyncJokeTask() throws Exception {
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.jokeText)).check(matches(withText("Oh Hi Mark! Here's a joke...This is a Joke.")));
+    }
+}
+
